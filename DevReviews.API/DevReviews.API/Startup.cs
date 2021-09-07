66761b936529fc090fb41682917876659e2c5784
Thiago Correa da Fonseca <thiagocorreafonseca@gmail.com>
2021-09-06 23:44:20 -0300

    adding swagger

--- DevReviews.API/DevReviews.API/Startup.cs
+++ DevReviews.API/DevReviews.API/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
+using Microsoft.OpenApi.Models;
+
 
 namespace DevReviews.API
 {
@@ -25,6 +21,11 @@ namespace DevReviews.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExemploAPI", Version = "v1" });
+            });
+
             services.AddControllers();
         }
 
@@ -34,6 +35,8 @@ namespace DevReviews.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevReviews.API v1"));
             }
 
             app.UseHttpsRedirection();
